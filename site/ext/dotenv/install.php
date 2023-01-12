2f695bba668d8ae46032d8fe552303d5ae04c3a3
Alex Petrov <alexmaoczedun@gmail.com>
2023-01-12 18:43:54 +0200

    bug fix with prod database

--- site/ext/dotenv/install.php
+++ site/ext/dotenv/install.php
@@ -2,4 +2,6 @@
 require_once __DIR__ . '/Loader.php';
 require_once __DIR__ . '/Retriever.php';
 require_once __DIR__ . '/functions.php';;
-(new Loader)->load('.env');
+
+//app.pavluks-trans.com
+(new Loader)->load((isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] === 'app.pavluks-trans.com') ? '.env.prod' : '.env');
